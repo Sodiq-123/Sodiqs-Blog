707271751258676eec4e9949e4d04e2b36bff348
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-17 18:02:32 +0100

    Modified

--- flask/app/email.py
+++ flask/app/email.py
@@ -10,4 +10,21 @@ def send_async_email(app, msg):
 
 def send_email(to, subject,template, **kwargs):
     app = current_app._get_current_object()
-    msg = Message(app.config[''])
+    msg = Message(subject="Hello", sender=app.config.get['MAIL_USERNAME'], 
+                    recepients=['sodiq.agunbiade.4@gmail.com'], 
+                    body=render_template('new_user.html'))
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
+
+# def send_email(to, subject, template, **kwargs):
+#     app = current_app._get_current_object()
+#     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+#                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+#     msg.body = render_template(template + '.txt', **kwargs)
+#     msg.html = render_template(template + '.html', **kwargs)
+#     thr = Thread(target=send_async_email, args=[app, msg])
+#     thr.start()
+#     return thr
