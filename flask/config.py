312e793c9fa90c9777a9b3170cc9d35267d60de5
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-18 01:05:27 +0100

    Added configuration for the app

---
+++ flask/config.py
@@ -0,0 +1,43 @@
+import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASK_MAIL_SUBJECT_PREFIX = 'Hello, '
+    FLASK_MAIL_SENDER = 'sodiq.agunbiade.4@gmail.com'
+    FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+         'sqlite:///' + os.path.join(basedir, 'data-dev.splite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+         'sqlite:///' + os.path.join(basedir, 'data-dev.splite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+         'sqlite:///' + os.path.join(basedir, 'data-dev.splite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
