09202b1fd2882ff50c7f8f09b6929068a5195d2e
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-22 17:21:17 +0100

    Modified

--- flask/app/models.py
+++ flask/app/models.py
@@ -18,8 +18,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
 
     @property
     def password(self):
