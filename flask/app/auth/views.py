61f52a2aeff43a469e400a2e302e5c3c78e9343c
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-23 11:12:50 +0100

    Modified

--- flask/app/auth/views.py
+++ flask/app/auth/views.py
@@ -31,7 +31,9 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email.form.email.data, username=form.username.data, password=form.password.data)
+        user = User(email = form.email.data.lower(), 
+                    username=form.username.data, 
+                    password=form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('You can now login')
