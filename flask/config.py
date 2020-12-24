cb6bebce35f0f9de8306f67d9a675c8f6e3838d9
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-24 12:12:12 +0100

    Modified

--- flask/config.py
+++ flask/config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sprhqfwh1@#$**dasjh19qhb2@'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
