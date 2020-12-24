c0d91b2c9f1b232aebc21fecc0927532eeaacceb
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-24 18:28:00 +0100

    Modified

--- flask/config.py
+++ flask/config.py
@@ -6,10 +6,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'amkadi!*aj01u9208bub!@*'
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true').lower() in ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'sodiq.agunbiade.4@gmail.com'
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'Temitope123*#'
-    # FLASK_MAIL_SUBJECT_PREFIX = 'Hello, '
+    FLASK_MAIL_SUBJECT_PREFIX = 'Hello, '
     # FLASK_MAIL_SENDER = 'sodiq.agunbiade.4@gmail.com'
     # FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
