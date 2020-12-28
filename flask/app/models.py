4cd2d4f2d2c8fd3b50243fc82dcb1b9ad8f734ed
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-12-28 18:13:14 +0100

    Modified

--- flask/app/models.py
+++ flask/app/models.py
@@ -35,9 +35,6 @@ class User(UserMixin, db.Model):
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     def generate_confirmation_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'confirm': self.id}).decode('utf-8')
@@ -53,6 +50,23 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @login_manager.user_loader
 def load_user(user_id):
