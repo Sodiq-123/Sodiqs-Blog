2678b5531ae9cefb7ae3fbc4c742185d3ffe50e0
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-01-01 19:05:52 +0100

    Modified

--- flask/config.py
+++ flask/config.py
@@ -12,7 +12,7 @@ class Config:
     # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASK_MAIL_SUBJECT_PREFIX = 'Hello, '
     MAIL_SENDER = 'sodiq.agunbiade.4@gmail.com'
-    BLOG_ADMIN = os.environ.get('BLOG_ADMIN')
+    BLOG_ADMIN = os.environ.get('BLOG_ADMIN
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
