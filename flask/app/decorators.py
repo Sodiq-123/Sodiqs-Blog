eb33b4986b7a6c5dffd5517ba853c13a849822d8
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-01-08 13:52:59 +0100

    fixed bugs

--- flask/app/decorators.py
+++ flask/app/decorators.py
@@ -1,7 +1,7 @@
 from functools import wraps
 from flask import abort 
 from .models import Permission
-
+from flask_login import current_user
 
 def permission_required(permission):
     def decorator(f):
--- flask/app/main/views.py
+++ flask/app/main/views.py
@@ -20,10 +20,10 @@ def index():
     page = request.args.get('page', 1, type=int)
     pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['BLOG_POSTS_PER_PAGE'],
-        error_out=False
-    )
+        error_out=False)
     posts = pagination.items
-    return render_template('index.html', form=form, posts=posts, pagination=pagination)
+    return render_template('index.html', form=form, posts=posts,
+                           pagination=pagination)
 
 
 @main.route('/user/<username>')
@@ -37,7 +37,7 @@ def user(username):
     return render_template('user.html', user=user, posts=posts,
                            pagination=pagination)
 
-                           
+
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
@@ -50,12 +50,13 @@ def edit_profile():
         db.session.commit()
         flash('Your profile has been updated.')
         return redirect(url_for('.user', username=current_user.username))
-    form.name.data = current_user.name 
+    form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', form=form)
 
-@main.route('/edit-profile<int:id>', methods=['GET', 'POST'])
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
 @login_required
 @admin_required
 def edit_profile_admin(id):
@@ -64,20 +65,20 @@ def edit_profile_admin(id):
     if form.validate_on_submit():
         user.email = form.email.data
         user.username = form.username.data
-        user.confirmed = form.comfirmed.data 
+        user.confirmed = form.confirmed.data
         user.role = Role.query.get(form.role.data)
         user.name = form.name.data
-        user.location = form.loaction.data
+        user.location = form.location.data
         user.about_me = form.about_me.data
         db.session.add(user)
         db.session.commit()
         flash('The profile has been updated.')
         return redirect(url_for('.user', username=user.username))
     form.email.data = user.email
-    form.username.data = user.username 
+    form.username.data = user.username
     form.confirmed.data = user.confirmed
     form.role.data = user.role_id
     form.name.data = user.name
-    form.location.data = user.location 
+    form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
