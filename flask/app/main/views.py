d8195ced1def4071e26e47905a7fee73286e19db
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-01-17 16:50:07 +0100

    Modified

--- flask/app/main/views.py
+++ flask/app/main/views.py
@@ -21,16 +21,16 @@ def index():
     show_followed = False
     if current_user.is_authenticated:
         show_followed = bool(request.cookies.get('show_followed', ''))
-        if show_followed:
-            query = current_user.followed_posts
-        else:
-            query = Post.query
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+    if show_followed:
+        query = current_user.followed_posts
+    else:
+        query = Post.query
+    pagination = query.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['BLOG_POSTS_PER_PAGE'],
         error_out=False)
     posts = pagination.items
     return render_template('index.html', form=form, posts=posts,
-                           show_followed=show_followed ,pagination=pagination)
+                           show_followed=show_followed, pagination=pagination)
 
 
 @main.route('/user/<username>')
@@ -101,7 +101,8 @@ def post(id):
 @login_required
 def edit(id):
     post = Post.query.get_or_404(id)
-    if current_user != post.author and not current_user.can(Permission.ADMIN):
+    if current_user != post.author and \
+            not current_user.can(Permission.ADMIN):
         abort(403)
     form = PostForm()
     if form.validate_on_submit():
@@ -127,7 +128,7 @@ def follow(username):
         return redirect(url_for('.user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash(f'You are now following {username}.')
+    flash('You are now following %s.' % username)
     return redirect(url_for('.user', username=username))
 
 
@@ -144,7 +145,7 @@ def unfollow(username):
         return redirect(url_for('.user', username=username))
     current_user.unfollow(user)
     db.session.commit()
-    flash(f'You are not following {username} anymore.')
+    flash('You are not following %s anymore.' % username)
     return redirect(url_for('.user', username=username))
 
 
@@ -152,18 +153,17 @@ def unfollow(username):
 def followers(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('Invlaid user.')
+        flash('Invalid user.')
         return redirect(url_for('.index'))
     page = request.args.get('page', 1, type=int)
     pagination = user.followers.paginate(
         page, per_page=current_app.config['BLOG_FOLLOWERS_PER_PAGE'],
-        error_out=False
-    )
-    follows = [{'user':item.follower, 'timestamp': item.timestamp}
-                for item in pagination.items]
-    return render_template('followers.html', user=user, title='Followers of',
-                            endpoint='.followers', pagination=pagination,
-                            follows=follows)
+        error_out=False)
+    follows = [{'user': item.follower, 'timestamp': item.timestamp}
+               for item in pagination.items]
+    return render_template('followers.html', user=user, title="Followers of",
+                           endpoint='.followers', pagination=pagination,
+                           follows=follows)
 
 
 @main.route('/followed_by/<username>')
@@ -181,6 +181,7 @@ def followed_by(username):
     return render_template('followers.html', user=user, title="Followed by",
                            endpoint='.followed_by', pagination=pagination,
                            follows=follows)
+
 
 @main.route('/all')
 @login_required
