432ae21e854adc919e51b265a3eb2663f1535603
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-01-20 17:47:34 +0100

    Modified

--- flask/config.py
+++ flask/config.py
@@ -6,8 +6,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    # MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASK_MAIL_SUBJECT_PREFIX = 'Hello, '
